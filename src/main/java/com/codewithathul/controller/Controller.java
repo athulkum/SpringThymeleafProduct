e4b54084075d7a74c63659dde9836e0487ecc9e6
athulkum <157148015+athulkum@users.noreply.github.com>
2025-03-26 16:58:24 +0530

    Add files via upload

--- src/main/java/com/codewithathul/controller/Controller.java
+++ src/main/java/com/codewithathul/controller/Controller.java
@@ -1,9 +1,14 @@
 package com.codewithathul.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.codewithathul.entity.Product;
 import com.codewithathul.repo.ProductRepository;
@@ -24,14 +29,17 @@ public class Controller {
 		product.setPrice(300.0);
 		product.setQty(1);
 		*/
-		model.addAttribute("product", product);
-		
-		
+		model.addAttribute("product", product);	
 		return "index";
 	}
 	
 	@PostMapping("/save")
-	public String handleSave(Product product ,Model model) {
+	public String handleSave(@Validated Product product, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+			
+			return "index";
+		}
        
 	Product p =	repository.save(product);
 	if(p.getId()!=null) {
@@ -40,5 +48,39 @@ public class Controller {
 		
 		return "index";
 	}
+
+	@GetMapping("/products")
+	public String getAllProducts(Model model) {
+		
+		model.addAttribute("products", repository.findAll());
+		
+		return "products";
+	}
 	
+	@GetMapping("/edit")
+	public String updateProduct(@RequestParam ("id") Integer id, Model model) {
+	
+		Optional<Product> p= repository.findById(id);
+		if(p.isPresent()) {
+	       Product product = p.get();
+			repository.save(product);
+			model.addAttribute("product", product);
+		}
+	return "index";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("id") Integer id, Model model) {
+		
+	Optional< Product> p=	repository.findById(id);
+	
+	if(p.isPresent()){
+		repository.deleteById(id);	
+		
+		model.addAttribute("msg", "delete Success");
+		model.addAttribute("products", repository.findAll());
+	}
+	return "products";
+	
+	}
 }
--- src/main/java/com/codewithathul/entity/Product.java
+++ src/main/java/com/codewithathul/entity/Product.java
@@ -3,6 +3,11 @@ package com.codewithathul.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name = "productform")
 public class Product {
 
 	
@@ -18,10 +24,16 @@ public class Product {
 	@GeneratedValue
 	private Integer id;
 	
+	@NotBlank (message = "Name cannot be blank")
+	@Size (min=3, max = 15, message = "Name should be min 4 and max 15")
 	private String name;
 	
+	@NotNull(message = "Price cannot be empty")
+	@Positive (message = "price value should be positive")
 	private Double price;
 	
+	@NotNull(message = "Quantity cannot be empty")
+	@Positive(message = "Quantity Should be positive")
 	private Integer qty;
 	
 }
